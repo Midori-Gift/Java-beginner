bf37b4912a4edd37dd71ad7c05257c8aa90cf823
Midori-Gift <okometaberoyo5009@gmail.com>
2021-08-17 15:30:06 +0900

    1章の1修了

---
+++ chapter-1/1-5-3練習問題.java
@@ -0,0 +1,8 @@
+
+public static void main(String[] args) {
+  boolean iserror = true;
+  byte byte01 ='駆';
+  final double pi = 3.14;
+  long　long01 = 314159265939979L;
+  String string01 = "みなとの攻撃！敵に15ポイントのダメージ";
+}
---
+++ chapter-1/1-5練習問題.java
@@ -0,0 +1,10 @@
+
+public static void main(String[] args) {
+  int a = 3;
+  int b = 5;
+
+  c = (a * b );
+
+  System.out.println("立幅" + a + "横幅" + b + "長方形の面積は" + c);
+
+}
