aee41c42cc3e1faa2300af5f908cbbf0aa21045e
Midori-Gift <okometaberoyo5009@gmail.com>
2021-08-19 20:19:59 +0900

    8/19修了

---
+++ chapter-6/java-beginner/java-practiceJVD/src/practiceJVD/Calclogic.java
@@ -0,0 +1,10 @@
+package practiceJVD;
+
+public class Calclogic {
+	  public static int tasu(int a, int b) {
+	    return (a + b);
+	  }
+	  public static int hiku(int a, int b) {
+	    return (a - b);
+	  }
+	}
