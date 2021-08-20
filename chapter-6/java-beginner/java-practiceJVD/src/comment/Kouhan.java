3b348313ab0765ea54e62609f1c9a59041f6393b
Midori-Gift <okometaberoyo5009@gmail.com>
2021-08-20 14:40:20 +0900

    練習問題6.8-1回答

---
+++ chapter-6/java-beginner/java-practiceJVD/src/comment/Kouhan.java
@@ -0,0 +1,14 @@
+package comment;
+
+public class Kouhan {
+	//	メソッド3つ目
+	 public static void callDeae() {
+		    System.out.println("えぇい、こしゃくな。くせ者だ！であえい！");
+		  }
+	//  メソッド4つ目 
+	public static void showMondokoro() throws Exception {
+		System.out.println("飛車さん、角さん。もういいでしょう。");
+		System.out.println("この紋所が目に入らぬか！");
+		Zenhan.doTogame();
+	}
+}
