b793aa529ab034ed4b6e1cad4b956fa553948aee
Midori-Gift <okometaberoyo5009@gmail.com>
2021-08-20 16:32:52 +0900

    コンパイル類のコミット

--- chapter-6/java-beginner/java-practiceJVD/src/comment/Kouhan.java
+++ chapter-6/java-beginner/java-practiceJVD/src/comment/Kouhan.java
@@ -2,13 +2,27 @@ package comment;
 
 public class Kouhan {
 	//	メソッド3つ目
-	 public static void callDeae() {
-		    System.out.println("えぇい、こしゃくな。くせ者だ！であえい！");
-		  }
-	//  メソッド4つ目 
+	public static void callDeae() {
+		System.out.println("えぇい、こしゃくな。くせ者だ！であえい！");
+	}
+
+	//  メソッド4つ目
 	public static void showMondokoro() throws Exception {
 		System.out.println("飛車さん、角さん。もういいでしょう。");
 		System.out.println("この紋所が目に入らぬか！");
+		class PrimeThread extends Thread {
+			long minPrime;
+
+			PrimeThread(long minPrime) {
+				this.minPrime = minPrime;
+			}
+
+			public void run() {
+				// compute primes larger than minPrime
+				PrimeRun p = new PrimeRun(1);
+				new Thread(p).start();
+			}
+		}
 		Zenhan.doTogame();
 	}
 }
---
+++ chapter-6/java-beginner/java-practiceJVD/src/comment/PrimeRun.java
@@ -0,0 +1,13 @@
+package comment;
+
+class PrimeRun implements Runnable {
+    long minPrime;
+    PrimeRun(long minPrime) {
+        this.minPrime = minPrime;
+    }
+
+    public void run() {
+        // compute primes larger than minPrime
+    }
+}
+
