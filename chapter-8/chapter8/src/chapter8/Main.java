c7bed63f5cf844853925a646a6abcd4e7a277f0e
Midori-Gift <okometaberoyo5009@gmail.com>
2021-08-21 11:48:35 +0900

    戦闘ログ

--- chapter-8/chapter8/src/chapter8/Main.java
+++ chapter-8/chapter8/src/chapter8/Main.java
@@ -8,15 +8,20 @@ public class Main {
 		h.name = "ミナト";
 		h.Hp = 100;
 
-//		お化けキノコを生成
+//		お化けキノコAを生成
 		Matango m1 = new Matango();
 		m1.Hp = 50;
 		m1.suffix = 'A';
 
-		System.out.println("勇者" + h.name + "生み出しました");
-		//		  勇者を呼び出してゆく
-		h.sit(5);
+//		お化けキノコBを生成
+		Matango m2 = new Matango();
+		m2.Hp = 50;
+		m2.suffix = 'B';
+
+//		冒険の始まり
 		h.slip();
-		h.sit(25);
+		m1.run();
+		m2.run();
 		h.run();
+	}
 }
--- chapter-8/chapter8/src/chapter8/Matango.java
+++ chapter-8/chapter8/src/chapter8/Matango.java
@@ -3,4 +3,9 @@ package chapter8;
 public class Matango {
 	int Hp;
 	final int LEVEL = 10;
+	char suffix;
+	public void run() {
+		System.out.println
+		("お化けキノコ" + this.suffix + "は逃げ出した");
+	}
 }
