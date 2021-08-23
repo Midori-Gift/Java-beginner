359e26d0584d39594991238e23304fec61e1e7c7
Midori-Gift <okometaberoyo5009@gmail.com>
2021-08-23 12:04:53 +0900

    1章問題下準備

---
+++ chapter-11/practice11/src/practice11/TrangibleAsset.java
@@ -0,0 +1,13 @@
+package practice11;
+
+public interface TrangibleAsset {
+//	public abstract String getName();
+//	public abstract int getPrice();
+//	public abstract String getColor();
+//	public abstract String getMakerName();
+
+	String getName();
+	int getPrice();
+	String getColor();
+	String getMakerNmae();
+}
