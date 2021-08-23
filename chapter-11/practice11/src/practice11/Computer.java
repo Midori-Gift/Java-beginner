d61f2402da4baef60b6db1ec0388737cef20c22a
Midori-Gift <okometaberoyo5009@gmail.com>
2021-08-23 14:01:54 +0900

    練習問題11.5-1分からず

--- chapter-11/practice11/src/practice11/Computer.java
+++ chapter-11/practice11/src/practice11/Computer.java
@@ -1,5 +1,19 @@
 package practice11;
 
-public class Computer {
+public class Computer implements TrangibleAsset{
 
-}
+	  String makerName;
+	  // コンストラクタ
+	  public Computer(String name, int price, String color, String makerName) {
+	  this.name = name;
+	  this.price = price;
+	  this.color = color;
+	  this.makerName = makerName;
+	  }
+	  // メソッド
+	  public String getName() { return this.name; }
+	  public int getPrice() { return this.price; }
+	  public String getColor() { return this.color; }
+	  public String getMakerName() { return this.makerName; }
+	}
+
