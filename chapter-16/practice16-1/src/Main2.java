0d5e4aa97ae375e4bc32e0de898ab9edeed1c258
Midori-Gift <okometaberoyo5009@gmail.com>
2021-09-01 21:00:29 +0900

    16章修了

---
+++ chapter-16/practice16-1/src/Main2.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class Main2 {
+  public static void main(String[] args) {
+    Hero h1 = new Hero("斎藤");
+    Hero h2 = new Hero("鈴木");
+    // List<Hero> heroes = new ArrayList<Hero>();
+    Map<String Integer> heros = new HashMap<String, Integer>();
+    // heroes.add(h1);
+    // heroes.add(h2);
+    heros.put("斎藤", 3);
+    heros.put("鈴木", 7);
+
+    String sa = prefs.get(3);
+    String sz = prefs.get(7);
+    int saito = prefs.get("斎藤");
+    int suzuki = prefs.get("鈴木");
+    for (Hero h : heroes) {
+        System.out.println();
+    }
+  }
+}
