ba8f2aefa6a657be2b5cf79fbc4f3f7db4f35f02
Midori-Gift <okometaberoyo5009@gmail.com>
2021-09-02 13:30:19 +0900

    練習問題17.9-1~2まで

---
+++ chapter-17/practice/src/practice/Main.java
@@ -0,0 +1,18 @@
+package practice;
+
+public class Main {
+
+	public static void main(String[] args) {
+		String s = new String();
+		s = null;
+		try {
+		System.out.println(s.length());
+		} catch (NullPointerException e) {
+			System.out.println("NullPointerException例外をcatchしました");
+		System.out.println("(ここから)--");
+		e.printStackTrace();
+		System.out.println("(ここまで)--");
+		}
+
+	}
+}
